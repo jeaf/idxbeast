87d02e9cae819672b2f5d6107bcd9266387f61b4
Francois Jeannotte <francois.jeannotte@gmail.com>
2013-01-08 01:24:20 -0500

    Put expected first, take into account that one-letter words are no longer indexed

--- test.py
+++ test.py
@@ -8,14 +8,14 @@ def assert_eq(expected, actual):
 start_time = time.clock()
 
 for i in range(10):
-  results = idxbeast.search('shared_ptr i e a de boost alpha')
-  assert_eq(len(results), 1)
+  results = idxbeast.search('shared_ptr de boost alpha')
+  assert_eq(2, len(results))
 
   results = idxbeast.search('for')
-  assert_eq(len(results), 11892)
+  assert_eq(11892, len(results))
 
   results = idxbeast.search('lambda american')
-  assert_eq(len(results), 2)
+  assert_eq(2, len(results))
 
 elapsed_time = time.clock() - start_time
 print 'All tests passed in {}.'.format(datetime.timedelta(seconds=elapsed_time))
