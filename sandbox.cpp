ccff25922885a12d22c181470664cec07538e88f
Francois Jeannotte <francois.jeannotte@intel.com>
2015-01-19 10:08:14 -0600

    Take snapshot of sandbox template experiments

--- sandbox.cpp
+++ sandbox.cpp
@@ -4,38 +4,82 @@
 #include <type_traits>
 #include <vector>
 
+#define TL2(T0, T1) TypeList<T0, TypeList<T1, NullType> >
+#define TL3(T0, T1, T2) TypeList<T0, TypeList<T1, TypeList<T2, NullType> > >
+
 using namespace std;
 
-template <int I>
-struct Int2Type
+struct NullType;
+
+template <int I> struct Int2Type{};
+
+template <typename H, typename T>
+struct TypeList
 {
-    enum { value = I };
+    typedef H head;
+    typedef T tail;
 };
 
-template <int I, typename T>
-struct Col
+
+template <typename TL, int I> struct TypeAt;
+template <typename H, typename T>
+struct TypeAt<TypeList<H, T>, 0>
 {
-    T get()
+    typedef H type;
+};
+template <typename H, typename T, int I>
+struct TypeAt<TypeList<H, T>, I>
+{
+    static_assert(!is_same<T, NullType>::value, "Invalid TypeAt index");
+    typedef typename TypeAt<T, I - 1>::type type;
+};
+
+template <typename TL> struct Length;
+template <> struct Length<NullType>
+{
+    enum { value = 0 };
+};
+template <typename H, typename T>
+struct Length<TypeList<H, T>>
+{
+    enum { value = 1 + Length<T>::value };
+};
+
+template <typename TL, int I = 0>
+struct Col : Col<typename TL::tail, I + 1>
+{
+    template <typename R>
+    R get(Int2Type<I>)
     {
-        return T();
+        return R();
+    }
+
+    template <typename R, int J>
+    R get(Int2Type<J> tag)
+    {
+        return Col<typename TL::tail, I + 1>::template get<R>(tag);
     }
 };
 
-template <typename T0, typename T1>
-struct Stmt
+template <int I> struct Col<NullType, I> {};
+
+template <typename TL>
+struct Stmt : Col<TL>
 {
-    Col<0, T0> col0;
-    Col<1, T1> col1;
+    template <int I>
+    typename TypeAt<TL, I>::type get()
+    {
+        typedef typename TypeAt<TL, I>::type RetType;
+        return Col<TL>::template get<RetType>(Int2Type<I>());
+    }
 };
 
 int main()
 {
-    //Stmt<int, string> s;
-    //cout << "0: " << s.get<0>() << endl;
-    ifstream in;
-    in.open("testdata_small.txt");
-    vector<char> b(16000);
-    in.read(b.data(), b.size());
+    Stmt<TL3(int, string, double)> s;
+    cout << "0: " << s.get<0>() << endl;
+    cout << "1: " << s.get<1>() << endl;
+    cout << "2: " << s.get<2>() << endl;
     return 0;
 }
 
