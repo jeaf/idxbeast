fec2f64e1370965074dbd24a85cde24ee17b055f
Francois Jeannotte <francois.jeannotte@intel.com>
2015-01-19 14:41:48 -0600

    Use integral_constant instead of Int2Type, add specific getters for types

--- sandbox.cpp
+++ sandbox.cpp
@@ -6,8 +6,6 @@
 
 using namespace std;
 
-template <int I> struct Int2Type{};
-
 template <int I, typename... Ts> struct TypeAt;
 template <typename H, typename... Ts>
 struct TypeAt<0, H, Ts...>
@@ -21,9 +19,24 @@ struct TypeAt<I, H, Ts...>
     typedef typename TypeAt<I - 1, Ts...>::type type;
 };
 template <int I>
-struct TypeAt<I>
+struct TypeAt<I> { static_assert(I < 0, "TypeAt index out of bounds"); };
+
+template <int I, typename T>
+struct ColGetter { static_assert(I < 0, "Unsupported type"); };
+template <int I>
+struct ColGetter<I, int64_t>
 {
-    static_assert(I < 0, "TypeAt index out of bounds");
+    static int64_t get() { return 3; }
+};
+template <int I>
+struct ColGetter<I, double>
+{
+    static double get() { return 999.111; }
+};
+template <int I>
+struct ColGetter<I, string>
+{
+    static string get() { return "abc"; }
 };
 
 template <int I, typename... Ts> struct Col {};
@@ -31,17 +44,18 @@ template <int I, typename H, typename... Ts>
 struct Col<I, H, Ts...> : Col<I + 1, Ts...>
 {
     template <typename R>
-    R get(Int2Type<I>)
+    R get(integral_constant<int, I>)
     {
-        return R();
+        return ColGetter<I, R>::get();
     }
 
     template <typename R, int J>
-    R get(Int2Type<J> tag)
+    R get(integral_constant<int, J> tag)
     {
         return Col<I + 1, Ts...>::template get<R>(tag);
     }
 };
+
 template <typename... Ts>
 struct Stmt : Col<0, Ts...>
 {
@@ -49,13 +63,13 @@ struct Stmt : Col<0, Ts...>
     typename TypeAt<I, Ts...>::type get()
     {
         typedef typename TypeAt<I, Ts...>::type RetType;
-        return Col<0, Ts...>::template get<RetType>(Int2Type<I>());
+        return Col<0, Ts...>::template get<RetType>(integral_constant<int, I>());
     }
 };
 
 int main()
 {
-    Stmt<int, double, string> s;
+    Stmt<int64_t, string, double> s;
     cout << "0: " << s.get<0>() << endl;
     cout << "1: " << s.get<1>() << endl;
     cout << "2: " << s.get<2>() << endl;
