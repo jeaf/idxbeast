80437e16f0a9401c6303dae1a78ec136803eb83f
Francois Jeannotte <francois.jeannotte@intel.com>
2015-01-19 14:10:21 -0600

    Use variadic template parameters instead of typelists

--- sandbox.cpp
+++ sandbox.cpp
@@ -4,49 +4,31 @@
 #include <type_traits>
 #include <vector>
 
-#define TL2(T0, T1) TypeList<T0, TypeList<T1, NullType> >
-#define TL3(T0, T1, T2) TypeList<T0, TypeList<T1, TypeList<T2, NullType> > >
-
 using namespace std;
-
-struct NullType;
 
 template <int I> struct Int2Type{};
 
-template <typename H, typename T>
-struct TypeList
-{
-    typedef H head;
-    typedef T tail;
-};
-
-
-template <int I, typename TL> struct TypeAt;
-template <typename H, typename T>
-struct TypeAt<0, TypeList<H, T>>
+template <int I, typename... Ts> struct TypeAt;
+template <typename H, typename... Ts>
+struct TypeAt<0, H, Ts...>
 {
     typedef H type;
 };
-template <int I, typename H, typename T>
-struct TypeAt<I, TypeList<H, T>>
+template <int I, typename H, typename... Ts>
+struct TypeAt<I, H, Ts...>
 {
-    static_assert(!is_same<T, NullType>::value, "Invalid TypeAt index");
-    typedef typename TypeAt<I - 1, T>::type type;
+    static_assert(I > 0, "TypeAt index negative");
+    typedef typename TypeAt<I - 1, Ts...>::type type;
+};
+template <int I>
+struct TypeAt<I>
+{
+    static_assert(I < 0, "TypeAt index out of bounds");
 };
 
-template <typename TL> struct Length;
-template <> struct Length<NullType>
-{
-    enum { value = 0 };
-};
-template <typename H, typename T>
-struct Length<TypeList<H, T>>
-{
-    enum { value = 1 + Length<T>::value };
-};
-
-template <int I, typename TL>
-struct Col : Col<I + 1, typename TL::tail>
+template <int I, typename... Ts> struct Col {};
+template <int I, typename H, typename... Ts>
+struct Col<I, H, Ts...> : Col<I + 1, Ts...>
 {
     template <typename R>
     R get(Int2Type<I>)
@@ -57,29 +39,27 @@ struct Col : Col<I + 1, typename TL::tail>
     template <typename R, int J>
     R get(Int2Type<J> tag)
     {
-        return Col<I + 1, typename TL::tail>::template get<R>(tag);
+        return Col<I + 1, Ts...>::template get<R>(tag);
     }
 };
-
-template <int I> struct Col<I, NullType> {};
-
-template <typename TL>
-struct Stmt : Col<0, TL>
+template <typename... Ts>
+struct Stmt : Col<0, Ts...>
 {
     template <int I>
-    typename TypeAt<I, TL>::type get()
+    typename TypeAt<I, Ts...>::type get()
     {
-        typedef typename TypeAt<I, TL>::type RetType;
-        return Col<0, TL>::template get<RetType>(Int2Type<I>());
+        typedef typename TypeAt<I, Ts...>::type RetType;
+        return Col<0, Ts...>::template get<RetType>(Int2Type<I>());
     }
 };
 
 int main()
 {
-    Stmt<TL3(int, string, double)> s;
+    Stmt<int, double, string> s;
     cout << "0: " << s.get<0>() << endl;
     cout << "1: " << s.get<1>() << endl;
     cout << "2: " << s.get<2>() << endl;
+    //cout << "3: " << s.get<3>() << endl;
     return 0;
 }
 
