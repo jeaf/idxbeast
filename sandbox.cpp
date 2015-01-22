3d5597012db5891a21304ea052e26dc2937d44a5
Francois Jeannotte <francois.jeannotte@intel.com>
2015-01-22 09:51:50 -0600

    Define a ColSpec class to group ColDef types, in preparation of adding BindSpec

--- sandbox.cpp
+++ sandbox.cpp
@@ -13,6 +13,9 @@
 
 using namespace std;
 
+//////////////////////////////////////////////////////////////////////////////
+// TypeAt
+//////////////////////////////////////////////////////////////////////////////
 template <int I, typename... Ts> struct TypeAt;
 template <typename H, typename... Ts>
 struct TypeAt<0, H, Ts...>
@@ -28,15 +31,21 @@ struct TypeAt<I, H, Ts...>
 template <int I>
 struct TypeAt<I> { static_assert(I < 0, "TypeAt index out of bounds"); };
 
+//////////////////////////////////////////////////////////////////////////////
+// SqliteCol
+//////////////////////////////////////////////////////////////////////////////
 template <int I, typename T>
-struct ColGetter { static_assert(I < 0, "Unsupported type"); };
+struct SqliteCol { static_assert(I < 0, "Unsupported type"); };
 template <int I>
-struct ColGetter<I, int64_t> { static int64_t get() { return 3; } };
+struct SqliteCol<I, int64_t> { static int64_t get() { return 3; } };
 template <int I>
-struct ColGetter<I, double> { static double get() { return 999.111; } };
+struct SqliteCol<I, double> { static double get() { return 999.111; } };
 template <int I>
-struct ColGetter<I, string> { static string get() { return "abc"; } };
+struct SqliteCol<I, string> { static string get() { return "abc"; } };
 
+//////////////////////////////////////////////////////////////////////////////
+// ColDef
+//////////////////////////////////////////////////////////////////////////////
 template <int I, typename T>
 struct ColDef : integral_constant<int, I> { typedef T type; };
 
@@ -55,6 +64,9 @@ template <int I> struct ColDefLookup<I>
     typedef struct ErrorColDefNotFound result;
 };
 
+//////////////////////////////////////////////////////////////////////////////
+// Col
+//////////////////////////////////////////////////////////////////////////////
 template <int ColIdx, typename... Ts> struct Col{};
 template <int ColIdx, typename H, typename... Ts>
 struct Col<ColIdx, H, Ts...> : Col<ColIdx + 1, Ts...>
@@ -62,7 +74,7 @@ struct Col<ColIdx, H, Ts...> : Col<ColIdx + 1, Ts...>
     template <typename R>
     R get(integral_constant<int, H::value>)
     {
-        return ColGetter<ColIdx, R>::get();
+        return SqliteCol<ColIdx, R>::get();
     }
 
     template <typename R, int J>
@@ -72,8 +84,11 @@ struct Col<ColIdx, H, Ts...> : Col<ColIdx + 1, Ts...>
     }
 };
 
+//////////////////////////////////////////////////////////////////////////////
+// ColSpec
+//////////////////////////////////////////////////////////////////////////////
 template <typename... Ts>
-struct Stmt : Col<0, Ts...>
+struct ColSpec : Col<0, Ts...>
 {
     template <int ColDefId>
     typename ColDefLookup<ColDefId, Ts...>::result::type get()
@@ -91,12 +106,23 @@ struct Stmt : Col<0, Ts...>
     }
 };
 
+//////////////////////////////////////////////////////////////////////////////
+// Stmt
+//////////////////////////////////////////////////////////////////////////////
+template <typename Spec1>
+struct Stmt : Spec1
+{
+};
+
+//////////////////////////////////////////////////////////////////////////////
+// main
+//////////////////////////////////////////////////////////////////////////////
 int main()
 {
-    Stmt<ColDef<'c1'  , int64_t>,
-         ColDef<'c2'  , double >,
-         ColDef<'test', string >,
-         ColDef<'alfa', int64_t>> s;
+    Stmt<ColSpec<ColDef<'c1'  , int64_t>,
+                 ColDef<'c2'  , double >,
+                 ColDef<'test', string >,
+                 ColDef<'alfa', int64_t>>> s;
 
     cout << "c1: "   << s.get<'c1'>()   << endl;
     cout << "c2: "   << s.get<'c2'>()   << endl;
@@ -105,14 +131,18 @@ int main()
     cout << endl;
 
     enum ColIds {x1, x2};
-    Stmt<ColDef<x1, int64_t>,
-         ColDef<x2, double >> s2;
+    Stmt<ColSpec<ColDef<x1, int64_t>,
+                 ColDef<x2, double >>> s2;
 
     cout << "x1: "   << s2.get<x1>()      << endl;
     cout << "x2: "   << s2.get<x2>()      << endl;
     cout << "col1: " << s2.get_bycol<1>() << endl;
     cout << "col0: " << s2.get_bycol<0>() << endl;
     cout << endl;
+
+    Stmt<ColSpec<ColDef<x1, int64_t>,
+                 ColDef<x2, double >>> t;
+    cout << t.get<x1>() << endl;
 
     return 0;
 }
